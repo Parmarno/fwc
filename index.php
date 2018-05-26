cca3b630cbdbb0ec280ccdd66705bc46b87a320e
Parmarno <k.ekkarach@gmail.com>
2018-05-26 15:57:50 +0700

    add repo

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Football World Cups</title>
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Brand</a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
+								aria-expanded="false">Page 1 <span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="#" target="_blank">Page 1</a></li>
+								<li><a href="#" target="_blank">Page 1 With Popup 1</a></li>
+								<li><a href="#" target="_blank">Page 1 With Popup 2</a></li>
+							</ul>
+						</li>
+						<li><a href="#" target="_blank">Page 2</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
+								aria-expanded="false">Page 3 <span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="#" target="_blank">Page 3</a></li>
+								<li><a href="#" target="_blank">Page 3 With Popup 1</a></li>
+								<li><a href="#" target="_blank">Page 3 With Popup 2</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="assets/js/app.js"></script>
+	</body>
+</html>
