3f73736ea7cee1edeff98778a894aa1440ead76b
Parmarno <k.ekkarach@gmail.com>
2018-05-26 16:17:42 +0700

    complete page 1

--- index.php
+++ index.php
@@ -9,47 +9,55 @@
 		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 	<body>
+		<header>
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Football World Cup</a>
+					</div>
 
-		<nav class="navbar navbar-default">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Brand</a>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
+									aria-expanded="false">Page 1 <span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="page1.html" target="_blank">Page 1</a></li>
+									<li><a href="page1-1.html" target="_blank">Page 1 With Popup 1</a></li>
+									<li><a href="page1-2.html" target="_blank">Page 1 With Popup 2</a></li>
+								</ul>
+							</li>
+							<li><a href="#" target="_blank">Page 2</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
+									aria-expanded="false">Page 3 <span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="#" target="_blank">Page 3</a></li>
+									<li><a href="#" target="_blank">Page 3 With Popup 1</a></li>
+									<li><a href="#" target="_blank">Page 3 With Popup 2</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
 				</div>
+			</nav>
+		</header>
 
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
-								aria-expanded="false">Page 1 <span class="caret"></span>
-							</a>
-							<ul class="dropdown-menu">
-								<li><a href="#" target="_blank">Page 1</a></li>
-								<li><a href="#" target="_blank">Page 1 With Popup 1</a></li>
-								<li><a href="#" target="_blank">Page 1 With Popup 2</a></li>
-							</ul>
-						</li>
-						<li><a href="#" target="_blank">Page 2</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
-								aria-expanded="false">Page 3 <span class="caret"></span>
-							</a>
-							<ul class="dropdown-menu">
-								<li><a href="#" target="_blank">Page 3</a></li>
-								<li><a href="#" target="_blank">Page 3 With Popup 1</a></li>
-								<li><a href="#" target="_blank">Page 3 With Popup 2</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
+		<div class="container">
+			<div class="jumbotron">
+				<h1>Design HTML</h1>
+				<p>เลือกดูงานตามหน้าได้ที่เมนูด้านบน</p>
 			</div>
-		</nav>
+		</div>
 
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
